524f0c997148bd910bc4d5f16076ff6502360f48
Cune36 <curroch3006@gmail.com>
2025-05-25 19:07:25 +0200

    COMPLETO

--- src/fp/tipos/CalendarioExamenes.java
+++ src/fp/tipos/CalendarioExamenes.java
@@ -1,6 +1,18 @@
 package fp.tipos;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -29,5 +41,66 @@ public class CalendarioExamenes {
 				res += "\n";
 		}
 		return res;
+	}
+
+	public Map<String, Set<Examen>> getExamenesPorAula(){
+		Map<String, Set<Examen>> res = new HashMap<>();
+		for(Examen e: this.getExamenes()) {
+			for(Aula a:e.getAulas()) {
+				if(res.keySet().contains(a.nombre())) {
+					Set<Examen> conj = res.get(a.nombre());
+					conj.add(e);
+					res.put(a.nombre(), conj);
+				}else {
+					Set<Examen> conj = new HashSet<>();
+					conj.add(e);
+					res.put(a.nombre(), conj);
+				}
+			}
+		}
+		return res;
+	}
+	
+	public Examen getExamenMayorPorcentajeAsistentes(LocalTime t, String nombreAula) {
+		return this.getExamenes().stream()
+				.filter(x-> x.getFechaHora().toLocalTime().isAfter(t))
+				.filter(x-> this.getExamenesPorAula().get(nombreAula).contains(x))
+				.max(Comparator.comparing(Examen::getPorcentajeAsistentes))
+				.orElse(null);
+	}
+	
+	public SortedSet<String> getAulasExamenesTipo(TipoExamen tipo){
+		return this.getExamenes().stream()
+				.filter(x-> x.getTipo().equals(tipo))
+				.flatMap(x-> x.getAulas().stream())
+				.map(x-> x.nombre())
+				.collect(Collectors.toCollection(TreeSet::new));
+	}
+	public String getAulaMasOcupada(LocalDate fecha) {
+		Map<String, Set<Examen>> res =  getExamenesPorAula();
+		Function<Entry<String, Set<Examen>>, Long> funcion = x -> x.getValue().stream()
+				.filter(l-> l.getFechaHora().toLocalDate().equals(fecha))
+				.mapToLong(l-> l.getDuracion().toMinutes())
+				.sum();
+				
+		return res.entrySet().stream()
+				.max(Comparator.comparing(funcion))
+				.orElseThrow(NoSuchElementException::new)
+				.getKey();
+	}
+	public List<LocalDate> getFechasConMasAulasDe(Integer umbral){
+		/*
+		 * Creamos un map por fechas de los examenes
+		 * Para cada lista de examnes, lo convertimos en el numero de aulas totales
+		 */
+		Function<List<Examen>, Integer> contarAulas = x -> x.stream()
+				.mapToInt(l-> l.getAulas().size())
+				.sum();
+		return this.getExamenes().stream()
+				.collect(Collectors.groupingBy(x-> x.getFechaHora().toLocalDate(),
+						HashMap::new, Collectors.collectingAndThen(Collectors.toList(), contarAulas))).entrySet().stream()
+				.filter(x-> x.getValue()> umbral)
+				.map(x-> x.getKey())
+				.collect(Collectors.toList());
 	}
 }
--- src/fp/tipos/tests/TestExamen.java
+++ src/fp/tipos/tests/TestExamen.java
@@ -1,10 +1,18 @@
 package fp.tipos.tests;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import fp.tipos.CalendarioExamenes;
 import fp.tipos.Examen;
 import fp.tipos.FactoriaExamen;
+import fp.tipos.TipoExamen;
 
 public class TestExamen {
 	
@@ -12,12 +20,53 @@ public class TestExamen {
 		Stream<Examen> res = FactoriaExamen.leeCSV("./data/examenes.csv");
 		CalendarioExamenes cal = new CalendarioExamenes(res);
 		System.out.println("Examenes leidos: " + cal.getNumeroExamenes());
-		System.out.println("Lectura calendario: " + cal.toString());
+		//System.out.println("Lectura calendario: " + cal.toString());
 		return cal;
+	}
+	private static void TestgetExamenesPorAula(CalendarioExamenes calendario) {
+		System.out.println("Exámenes por aula (por motivos de espacio, solo se muestran las asignaturas): ");
+		Map<String, Set<Examen>> res = calendario.getExamenesPorAula();
+		for(String aula:res.keySet()) {
+			List<String> lista = res.get(aula).stream().map(x-> x.getAsignatura()).collect(Collectors.toList());
+			System.out.println("Aula " + aula + lista);
+		}
+	}
+	private static void TestgetExamenMayorPorcentajeAsistentes(CalendarioExamenes calendario,LocalTime t, String nombreAula) {
+		System.out.println("Examen con mayor porcentaje de asistentes realizado en el aula " + nombreAula +  "y con hora de comienzo posterior a las " + t + ":");
+		Examen res = calendario.getExamenMayorPorcentajeAsistentes(t, nombreAula);
+		System.out.println(res + "\n");
+	}
+	
+	private static void TestgetAulasExamenesTipo(CalendarioExamenes calendario,TipoExamen tipo) {
+		SortedSet<String> res = calendario.getAulasExamenesTipo(tipo);
+		System.out.println("ulas utilizadas en exámenes de tipo " + tipo + ": " + res + "\n");
+	}
+	private static void TestgetAulaMasOcupada(CalendarioExamenes calendario,LocalDate fecha) {
+		String res = calendario.getAulaMasOcupada(fecha);
+		System.out.println("Aula con mayor ocupación en la fecha " +fecha + ":" + res + "\n" );
+	}
+	
+	private static void TestgetFechasConMasAulasDe(CalendarioExamenes calendario,Integer umbral) {
+		List<LocalDate> res = calendario.getFechasConMasAulasDe(umbral);
+		System.out.println("Fechas con mas de " + umbral + " aulas: " + res);
 	}
 	
 	public static void main(String args[]) {
-		System.out.println("TestLeeCSV");
+		System.out.println("TestLeeCSV===============");
 		CalendarioExamenes calendario = TestLeeCSV();
+		System.out.println("EJERCICIO 4.1===================================================================");
+		TestgetExamenesPorAula(calendario);
+		System.out.println("EJERCICIO 4.2===================================================================");
+		TestgetExamenMayorPorcentajeAsistentes(calendario,  LocalTime.of(8, 30), "F1.30");
+		TestgetExamenMayorPorcentajeAsistentes(calendario,  LocalTime.of(15, 30), "I2.31");
+		System.out.println("EJERCICIO 4.3===================================================================");
+		TestgetAulasExamenesTipo(calendario, TipoExamen.PRACTICO);
+		TestgetAulasExamenesTipo(calendario, TipoExamen.TEORICO);
+		System.out.println("EJERCICIO 4.4===================================================================");
+		TestgetAulaMasOcupada(calendario, LocalDate.of(2024, 05, 25));
+		TestgetAulaMasOcupada(calendario, LocalDate.of(2024, 06, 07));
+		System.out.println("EJERCICIO 4.5===================================================================");
+		TestgetFechasConMasAulasDe(calendario, 5);
+		TestgetFechasConMasAulasDe(calendario, 8);
 	}
 }
